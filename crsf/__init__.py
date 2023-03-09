6e3d8ad6597b63386c56de35af9da2f6bf449def
JamesCordell <cordell.james@gmail.com>
2023-03-09 20:38:13 +0000

    names change

--- crsf/__init__.py
+++ crsf/__init__.py
@@ -1,7 +1,7 @@
 ##
 ## This file is part of the libsigrokdecode project.
 ##
-## Copyright (C) 2013 Uwe Hermann <uwe@hermann-uwe.de>
+## Copyright (C) 2013 James Cordell <james@cordell.org.uk>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
